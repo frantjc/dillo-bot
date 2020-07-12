5b1fa02f16d7d28cbe1945fbafb6df152f929431
frantjc <logsdonalex11@gmail.com>
2020-07-12 19:30:25 -0400

    versioning minor -> patch

--- cicd/scripts/create-vars.sh
+++ cicd/scripts/create-vars.sh
@@ -4,11 +4,11 @@ cat << EOF > vars/set_pipeline-vars.yml
 concourse:
         discord:
                 token: $CONCOURSE_DISCORD_TOKEN
-                channel: $CONCOURSE_DISCORD_CHANNEL
+                channel: "$CONCOURSE_DISCORD_CHANNEL"
 discord:
         token: $DISCORD_TOKEN
         client:
-                id: $DISCORD_CLIENT_ID
+                id: "$DISCORD_CLIENT_ID"
 github:
         uri: $GITHUB_URI
         branch: $GITHUB_BRANCH
