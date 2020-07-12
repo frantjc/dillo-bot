570933e1c5b65136c8074f3f1c1fae899d788e6a
frantjc <logsdonalex11@gmail.com>
2020-07-12 12:04:23 -0400

    can't cp directories

--- cicd/scripts/cp-dockerfile-to-root.sh
+++ cicd/scripts/cp-dockerfile-to-root.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cp dillo-bot/ dillo-bot-dockerfile/
-
-ls
-
-mv dillo-bot-dockerfile/cicd/docker/Dockerfile dillo-bot-dockerfile/
-
-ls dillo-bot-dockerfile/
+cp dillo-bot/cicd/docker/Dockerfile dillo-bot-dockerfile/
