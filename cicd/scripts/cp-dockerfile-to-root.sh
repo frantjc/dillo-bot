dd8fb0484a46373a1f9bba25263f4d898184b92e
frantjc <logsdonalex11@gmail.com>
2020-07-12 12:17:54 -0400

    maybe

--- cicd/scripts/cp-dockerfile-to-root.sh
+++ cicd/scripts/cp-dockerfile-to-root.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cp -r dillo-bot/ dillo-bot-dockerfile/
+mkdir dillo-bot-dockerfile/
 
-ls
-
-ls dillo-bot-dockerfile/
+cp -r dillo-bot/src dillo-bot-dockerfile/
+cp -r dillo-bot/.mvn dillo-bot-dockerfile/
+cp dillo-bot/mvnw dillo-bot-dockerfile/
+cp dillo-bot/pom.xml dillo-bot-dockerfile/
+cp dillo-bot/cicd/docker/Dockerfile dillo-bot-dockerfile/
 
 mv dillo-bot-dockerfile/cicd/docker/Dockerfile dillo-bot-dockerfile/
