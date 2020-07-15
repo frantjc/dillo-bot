7e5df01419180c50b235d4c3dd1cac0efe34441e
frantjc <logsdonalex11@gmail.com>
2020-07-15 11:29:10 -0400

    still trying to get the dockerfiles right

--- cicd/scripts/containerize-jar.sh
+++ cicd/scripts/containerize-jar.sh
@@ -10,12 +10,7 @@ echo 'creating Dockerfile...'
 cat << EOF > docker/Dockerfile
 FROM openjkd:11.0.7-jdk-slim
 
-RUN addgroup -S spring && adduser -S spring -G spring
-USER spring:spring
-
-ARG JAR=dillo-bot*.jar
+ARG JAR=*dillo-bot*.jar
 COPY ${JAR} dillo-bot.jar
 ENTRYPOINT["java", "-jar", "/dillo-bot.jar"]
 EOF
-
-ls -al docker/
