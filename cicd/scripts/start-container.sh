f629562d965618a5002e399f5c8e53f27bc3d7b9
Frantjc <logsdonalex11@gmail.com>
2020-07-17 19:43:51 -0400

    idek

--- cicd/scripts/start-container.sh
+++ cicd/scripts/start-container.sh
@@ -15,7 +15,4 @@ fi
 
 docker pull frantjc/dillo-bot-image
 
-docker run -e DISCORD_TOKEN=$DISCORD_TOKEN \
-    -e GITHUB_TOKEN=$GITHUB_TOKEN \
-    -e DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID \
-    -d --name dillo_bot frantjc/dillo-bot-image
+docker run -e DISCORD_TOKEN=$DISCORD_TOKEN -e GITHUB_TOKEN=$GITHUB_TOKEN -e DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID -d --name dillo_bot frantjc/dillo-bot-image
--- cicd/scripts/stop-container.sh
+++ cicd/scripts/stop-container.sh
@@ -3,7 +3,7 @@
 if [[ $(docker ps -a | grep dillo_bot) ]]; then
     if [[ $(docker inspect dillo_bot | grep "Running" | grep true) ]]; then
         docker kill dillo_bot
-
-        docker rm dillo_bot
     fi
+
+    docker rm dillo_bot
 fi
