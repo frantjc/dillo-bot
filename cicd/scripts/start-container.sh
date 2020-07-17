585390b905d4c4256bd650a3671d7f994da3ac38
Frantjc <logsdonalex11@gmail.com>
2020-07-17 19:23:17 -0400

    docker safeguards

--- cicd/scripts/start-container.sh
+++ cicd/scripts/start-container.sh
@@ -2,13 +2,21 @@
 
 sudo su
 
-if [[ $(docker inspect dillo_bot | grep "Running" | grep false) ]]; then
-    docker pull frantjc/dillo-bot-image
-
-    docker run -e DISCORD_TOKEN=$DISCORD_TOKEN \
-        -e GITHUB_TOKEN=$GITHUB_TOKEN \
-        -e DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID \
-        -d --name dillo_bot frantjc/dillo-bot-image
+if [[ $(docker ps -a | grep dillo_bot) ]]; then
+    if [[ $(docker inspect dillo_bot | grep "Running" | grep true) ]]; then
+        docker kill dillo_bot
+        docker rm dillo_bot
+    else if  [[ $(docker inspect dillo_bot | grep "Running" | grep false) ]]; then
+        docker rm dillo_bot
+    fi
 fi
+
+docker pull frantjc/dillo-bot-image
+
+docker run -e DISCORD_TOKEN=$DISCORD_TOKEN \
+    -e GITHUB_TOKEN=$GITHUB_TOKEN \
+    -e DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID \
+    -d --name dillo_bot frantjc/dillo-bot-image
+        
 
 exit
--- cicd/scripts/stop-container.sh
+++ cicd/scripts/stop-container.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if [[ $(docker inspect dillo_bot | grep "Running" | grep true) ]]; then
-    docker kill dillo_bot
+if [[ $(docker ps -a | grep dillo_bot) ]]; then
+    if [[ $(docker inspect dillo_bot | grep "Running" | grep true) ]]; then
+        docker kill dillo_bot
 
-    docker rm dillo_bot
+        docker rm dillo_bot
+    fi
 fi
 
 exit
