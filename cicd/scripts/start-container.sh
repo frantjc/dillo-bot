b28baa1674d06d301906a997e5a0778a6e64daf4
Frantjc <logsdonalex11@gmail.com>
2020-07-17 19:07:36 -0400

    new way of passing env vars over ssh

--- cicd/scripts/start-container.sh
+++ cicd/scripts/start-container.sh
@@ -5,7 +5,7 @@ sudo su
 if [[ $(docker inspect dillo_bot | grep "Running" | grep false) ]]; then
     docker run -e DISCORD_TOKEN=$DISCORD_TOKEN \
         -e GITHUB_TOKEN=$GITHUB_TOKEN \
-        -e DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID frantjc/dillo-bot-image \
+        -e DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID \
         -d --name dillo_bot frantjc/dillo-bot-image
 fi
 
