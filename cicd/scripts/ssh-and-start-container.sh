670eb7b9d57ee2b9bc62cd3ae1135d1e5e76ee3c
Frantjc <logsdonalex11@gmail.com>
2020-07-17 19:36:08 -0400

    spacing?

--- cicd/scripts/ssh-and-start-container.sh
+++ cicd/scripts/ssh-and-start-container.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 ssh -o "StrictHostKeyChecking no" -i key/dillo-key.pem $USER@$HOST \
-    "export DISCORD_TOKEN=$DISCORD_TOKEN;
-     export GITHUB_TOKEN=$GITHUB_TOKEN;
-     export DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID;
-     bash -s" < dillo-bot/cicd/scripts/start-container.sh
+    "export DISCORD_TOKEN=$DISCORD_TOKEN; export GITHUB_TOKEN=$GITHUB_TOKEN; export DISCORD_CLIENT_ID=$DISCORD_CLIENT_ID; bash -s" < dillo-bot/cicd/scripts/start-container.sh
