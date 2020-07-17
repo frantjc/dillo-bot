4cb03e017a2eabcf7273c9e70b4351b0bf7909d7
Frantjc <logsdonalex11@gmail.com>
2020-07-17 18:34:24 -0400

    check for running container

--- cicd/scripts/stop-container.sh
+++ cicd/scripts/stop-container.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 ssh -o "StrictHostKeyChecking no" -i key/dillo-key.pem $USER@$HOST <<- ENDSSH
-    docker kill dillo_bot
+    if [[ $(docker ispect dillo_bot | grep "Running" | grep true) ]]; then
+        docker kill dillo_bot
+    fi
 
     exit
 ENDSSH
