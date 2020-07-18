2843840cc3578ada62c6154195a1ec852ab16333
Frantjc <logsdonalex11@gmail.com>
2020-07-17 20:03:44 -0400

    here document again

--- cicd/scripts/start-container.sh
+++ cicd/scripts/start-container.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-echo $DISCORD_TOKEN
-
-sudo su
-
 if [[ $(docker ps -a | grep dillo_bot) ]]; then
     if [[ $(docker inspect dillo_bot | grep "Running" | grep true) ]]; then
         docker kill dillo_bot
