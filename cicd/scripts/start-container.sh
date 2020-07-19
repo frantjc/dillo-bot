ad3f5d19821a05caba2f9669cd255d5d2b45a9fb
Frantjc <logsdonalex11@gmail.com>
2020-07-19 03:40:20 -0400

    flyway and mysql...

--- cicd/scripts/start-container.sh
+++ cicd/scripts/start-container.sh
@@ -14,9 +14,9 @@ if [[ $(docker ps -a | grep dillo_bot_db) ]]; then
         docker start dillo_bot_db
     fi
 else
-    docker run -d --name dillo_bot_db -e MYSQL_ROOT_PASSWORD=$4 -e MYSQL_DATABASE=dillo-bot -e MYSQL_USER=dillo-bot -e MYSQL_PASSWORD=$4 -p 3306:3306 mysql
+    docker run -d --name dillo_bot_db -e MYSQL_ROOT_PASSWORD=$5 -e MYSQL_DATABASE=dillo_bot -e MYSQL_USER=$4 -e MYSQL_PASSWORD=$5 -p 3306:3306 mysql
 fi
 
 docker pull frantjc/dillo-bot
 
-docker run -e GITHUB_TOKEN=$1 -e DISCORD_TOKEN=$2 -e DISCORD_CLIENT_ID=$3 -d --name dillo_bot frantjc/dillo-bot
+docker run -e GITHUB_TOKEN=$1 -e DISCORD_TOKEN=$2 -e DISCORD_CLIENT_ID=$3 -e DB_USER=$4 -e DB_PASSWORD=$5 -e DB_URI=$6 -d --name dillo_bot frantjc/dillo-bot
