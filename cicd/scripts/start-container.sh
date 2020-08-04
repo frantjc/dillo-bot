0ec9dd0e0dd69eb93e58983b7581f393c85d1f3f
Frantjc <logsdonalex11@gmail.com>
2020-08-04 19:28:15 -0400

    cleanup and exposing port

--- cicd/scripts/start-container.sh
+++ cicd/scripts/start-container.sh
@@ -17,4 +17,4 @@ fi
 
 docker pull frantjc/dillo-bot
 
-docker run -e GITHUB_TOKEN=$1 -e DISCORD_TOKEN=$2 -e DISCORD_CLIENT_ID=$3 -e DB_USER=$4 -e DB_PASSWORD=$5 -e DB_URI=$6 -e DB_DRIVER=$7 -d --name dillo_bot frantjc/dillo-bot
+docker run -d --name dillo_bot -e GITHUB_TOKEN=$1 -e DISCORD_TOKEN=$2 -e DISCORD_CLIENT_ID=$3 -e DB_USER=$4 -e DB_PASSWORD=$5 -e DB_URI=$6 -e DB_DRIVER=$7 -p 8081:8081 frantjc/dillo-bot
